5a47f61591e68da40045f6c6044cacaa5b4c72cd
Pedro Silva <pedrossilva.br@gmail.com>
2018-01-24 22:48:27 -0200

    81. M2#A15 - TDD - filter recursivo

--- src/filter/filter-recursive.js
+++ src/filter/filter-recursive.js
@@ -2,9 +2,10 @@
 
 export const filter = (arr, func) => {
   return (function internalFilter(internalArr, count) {
-    if(!internalArr.length) return []
-    const [head, ...tail] = internalArr
-    const newTail = internalFilter(tail, count+1)
-    return !!func(head, count, arr) ? [head, ...newTail] : newTail
+    return !internalArr.length ? [] : (() => {
+      const [head, ...tail] = internalArr
+      const newTail = internalFilter(tail, count+1)
+      return !!func(head, count, arr) ? [head, ...newTail] : newTail
+    })()
   })(arr, 0)
 }
